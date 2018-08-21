f478b2ae324745f9bf464445f7ce85ad0071d6cc
Youp Koopmans <youpkoopmans@hotmail.com>
2018-08-21 22:24:40 +0200

    Fixed sidebar icons

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -12,13 +12,14 @@
             <div id="main-menu" class="main-menu collapse navbar-collapse">
                 <ul class="nav navbar-nav">
                     <h3 class="menu-title"><i class="menu-icon fa fa-tachometer"></i> &nbsp; @lang('site.dashboard')</h3>
+                    <li @if (\Request::is('issue*')) class="active" @endif><a href="{{ route('home') }}"><i class="menu-icon fa fa-crosshairs"></i>@lang('dashboard.issue')</a></li>
                     <li @if (\Request::is('status*')) class="active" @endif><a href="{{ route('status.index') }}"><i class="menu-icon fa fa-adjust"></i>@lang('dashboard.status')</a></li>
                     <li @if (\Request::is('priority*')) class="active" @endif><a href="{{ route('priority.index') }}"><i class="menu-icon fa fa-exclamation-circle"></i>@lang('dashboard.priority')</a></li>
-                    <li @if (\Request::is('user*')) class="active" @endif><a href="{{ route('user.index') }}"><i class="menu-icon fas fa-users-cog"></i>@lang('dashboard.user')</a></li>
-                    <li @if (\Request::is('client*')) class="active" @endif><a href="{{ route('client.index') }}"><i class="menu-icon fa fa-users"></i>@lang('dashboard.client')</a></li>
-                    <li @if (\Request::is('project*')) class="active" @endif><a href="{{ route('project.index') }}"><i class="menu-icon fas fa-project-diagram"></i>@lang('dashboard.project')</a></li>
+                    <li @if (\Request::is('user*')) class="active" @endif><a href="{{ route('user.index') }}"><i class="menu-icon fa fa-user"></i>@lang('dashboard.user')</a></li>
+                    <li @if (\Request::is('client*')) class="active" @endif><a href="{{ route('client.index') }}"><i class="menu-icon fa fa-user-circle-o"></i>@lang('dashboard.client')</a></li>
+                    <li @if (\Request::is('project*')) class="active" @endif><a href="{{ route('project.index') }}"><i class="menu-icon fa fa-pencil-square-o"></i>@lang('dashboard.project')</a></li>
                     <li @if (\Request::is('type*')) class="active" @endif><a href="{{ route('type.index') }}"><i class="menu-icon fa fa-bookmark"></i>@lang('dashboard.type')</a></li>
-                    <li @if (\Request::is('planning*')) class="active" @endif><a href="{{ route('planning.index') }}"><i class="menu-icon fas fa-calendar-alt"></i>@lang('site.planning')</a></li>
+                    <li @if (\Request::is('planning*')) class="active" @endif><a href="{{ route('planning.index') }}"><i class="menu-icon fa fa-calendar"></i>@lang('site.planning')</a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         @endif
